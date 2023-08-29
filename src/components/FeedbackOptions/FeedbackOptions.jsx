098edda6e1bbb07e7ab10cdc4215a3ef292a207e
Samcia <samantta689@gmail.com>
2023-08-29 14:46:35 +0200

    Feedback initial

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import './FeedbackOptions.css';
+
+class FeedbackOptions extends Component {
+  static defaultProps = {};
+
+  static PropTypes = {};
+
+  render() {
+    return;
+  }
+}
